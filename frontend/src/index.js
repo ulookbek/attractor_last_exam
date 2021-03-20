0df261e3dc013ea1d54f9f08895c5ea3a0b1a927
Улукбек Капарбеков <ulukbekkaparbekov@gmail.com>
2021-03-20 14:46:16 +0600

    #14 Подключил redux к фронтенду

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from "connected-react-router";
 import App from './App';
+import store, { history } from "./store/storeConfig";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const app = (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
 );
+
+ReactDOM.render(app, document.getElementById('root'));
